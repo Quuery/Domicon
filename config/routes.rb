6a16f5fa70bfd737becab2a3d943eeef0393ee34
Daniel Leitner <d4l.l5r@googlemail.com>
2012-10-01 22:18:34 +0200

    Add session routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Domicon::Application.routes.draw do
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
